82df130d974d1e57aee2ce1f7078ad6d54de8fef
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-04-17 00:32:21 +0300

    Add another localhost url to cors options

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ if (process.env.NODE_ENV === 'production') {
         origin: ['http://127.0.0.1:3000',
             'http://localhost:3000',
             'http://127.0.0.1:5173',
+            'http://localhost:5173',
             'http://localhost:5174',
             'http://localhost:5174',
             'http://localhost:5175'
